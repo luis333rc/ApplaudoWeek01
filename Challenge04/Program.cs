93da0aa7dbebaf8cc989ee527b1108e78f652411
Luis <luis.w.rivera.c@gmail.com>
2023-02-13 01:20:45 -0500

    create Facebook Post

---
+++ Challenge04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Challenge04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("Mike");
+            user.CreatePost(new Post("Mi 1er Post", "Mi mascota come demasiado") );
+
+            var post = user.GetLastPost();
+
+            Console.WriteLine("Enter names: ");
+
+            while (true)
+            {
+                var name = Console.ReadLine();
+                bool userQuit = string.IsNullOrWhiteSpace(name);
+                if (userQuit)
+                    break;
+
+                post.LikeBy(name);
+            }
+
+            Console.WriteLine( post.GetLikes() );
+        }
+    }
+}
